f2ba91697051c5a4d23b4b31af7e5b0447f2ae75
Alex Valcourt Caron <alex.valcourt.caron@usherbrooke.ca>
2017-03-19 14:39:05 -0400

    Cleaned Develop to make it worka as a base

--- mastawrap/eventManager/translationEventManager.py
+++
@@ -1,9 +0,0 @@
-from mastawrap.eventManager.eventManager import EventManager
-from mastawrap.eventManager.eventTemplateManager import require_template
-
-
-class TranslationEventManager(EventManager):
-
-    @require_template("translation")
-    def createTranslationEvent(self, templateId, translation):
-        self._addEvent("translation", translation)
